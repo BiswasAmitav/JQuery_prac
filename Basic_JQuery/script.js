b1b22b56233be876d160e523b01461cb5e7255a1
BiswasAmitav <biswasamit2648@gmail.com>
2023-01-23 22:44:40 +0530

    here i practiced some basic jwuery code

---
+++ Basic_JQuery/script.js
@@ -0,0 +1,15 @@
+// $('h1').hide(); to hide the element 
+
+
+$('h1').css('color','red');
+$('#para').css('color','green');
+
+$('li:odd').css('color','orange');//It changes the color based on the indexing number..
+
+
+$('li:nth-child(2)').css('color','gray')//It is used to select specific items ..
+
+$(':Password').css('color','red'); //Wtih type we can select or modify the 
+
+
+// How to select form element ...
